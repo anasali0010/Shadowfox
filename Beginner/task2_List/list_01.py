baa100d50955a985765ae8d69cbfea3ce6766a7a
anas <ali@anass-MacBook-Air.local>
2026-03-05 17:10:23 +0530

    task-completed-Calculate numbers of members in justice_league

---
+++ Beginner/task2_List/list_01.py
@@ -0,0 +1,4 @@
+justice_league = ["Superman","Batman","Wonder Woman","Flash","Aquaman","Green Lantern"]
+# calculate the member in  the justice_league.
+total_members=len(justice_league) # here we use len function to calculate the total members in the justice league.
+print("Total members in justice league are: ",total_members)
