b579a09f08ffa5cf43f571cfb28fe8d517aedd9c
anas <ali@anass-MacBook-Air.local>
2026-03-05 16:41:15 +0530

    task-Completed-Simple Interest

--- Beginner/task1_variables/variables_03.py
+++ Beginner/task1_variables/variables_03.py
@@ -4,4 +4,5 @@ P=eval(input("Enter the principal amount :"))
 R=eval(input("Enter the rate of interest : "))
 T=3
 SI=(P * R  * T)/100
+
 print(f"simple interest for the given principal amount {P} at rate of interest {R} % for {T} years is : {SI}")
