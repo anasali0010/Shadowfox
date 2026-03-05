7ba695b9b178c8543582e0bd6b072411fe1cf4b0
anas <ali@anass-MacBook-Air.local>
2026-03-05 16:54:13 +0530

    task-Completed-Keyword error

--- Beginner/task1_variables/variables_02.py
+++ Beginner/task1_variables/variables_02.py
@@ -1,3 +1,4 @@
 # creating a variable with name for and assigning a value to it  
 for = 4
-    # for is a reserved keyword in python and cannot be used as a variable name so this code will give a syntax error when we try to run it.
+    # for is a reserved keyword in python and cannot be used as a variable name .
+    # it shows synteax error when we try to run it .
