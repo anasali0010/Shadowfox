8b4242dd2c10d70ce4b10b50f1c519982bf7ebcd
anas <ali@anass-MacBook-Air.local>
2026-03-06 16:32:41 +0530

    task-Completed-Replacing existing list with new members.

---
+++ Beginner/task2_List/list_05.py
@@ -0,0 +1,15 @@
+# To assemble a new team .Replacing existing list  with new members.
+justice_league = ["Superman","Batman","Wonder Woman","Flash","Aquaman","Green Lantern"]
+# Replace the list with new members
+'''justice_league=['Superman', 'Cyborg', 'Shazam', 'Hawkgirl', 'Martian Manhunter', 'Green Arrow'] '''
+
+# Replacing existing list with new members.
+justice_league[1]="Cyborg"
+justice_league[2]="Shazam"
+justice_league[3]="Hawkgirl"
+justice_league[4]="Martian Manhunter"
+justice_league[5]="Green Arrow"
+
+print(justice_league)
+
+
