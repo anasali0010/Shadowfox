b4545eab3e92f71329e1a08a98fdaa20af62f4a1
anas <ali@anass-MacBook-Air.local>
2026-03-06 16:38:44 +0530

    task-Completed-Sorting justice_league members alphabeticaly.

---
+++ Beginner/task2_List/list_06.py
@@ -0,0 +1,5 @@
+# Sort the list of Justice League members in alphabetical order .
+justice_league=['Superman', 'Cyborg', 'Shazam', 'Hawkgirl', 'Martian Manhunter', 'Green Arrow'] 
+justice_league.sort()
+# Now Cyborg is the new leader of the justice league.
+print(justice_league)
