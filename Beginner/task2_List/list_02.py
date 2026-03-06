4db031914ec666d3c8ce38671cdfc154c70c964c
anas <ali@anass-MacBook-Air.local>
2026-03-06 15:36:21 +0530

    task-Completed-adding two new members

---
+++ Beginner/task2_List/list_02.py
@@ -0,0 +1,5 @@
+justice_league = ["Superman","Batman","Wonder Woman","Flash","Aquaman","Green Lantern"]
+# Batman recruited  Batgirl and Nightwing as neew members.
+justice_league.append("Batgirl")
+justice_league.append("Nightwing")
+print(justice_league)
