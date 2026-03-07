93271b8250cc4e348731fbed247707fd34571aea
anas <ali@anass-MacBook-Air.local>
2026-03-07 22:34:09 +0530

    task-Completed Creating a list that consist  friends name and name length

---
+++ Beginner/task3_Dictionary/dictionary_01.py
@@ -0,0 +1,12 @@
+Friends_name=['Anas','Faiz','Himanshu','Adeeb','Uzaif']
+friends_name_length =[]
+for name in Friends_name:
+ friends_name_length .append((name,len(name)))
+print(friends_name_length )
+# another method :
+'''
+Friends =['Anas','Faiz','Himanshu','Adeeb','Uzaif']
+friends_with_length =[(name,len(name)) for name in Friends ]
+print(friends_with_length)
+'''
+
